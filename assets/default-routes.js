49df73fb189d1051a72517841dde5bd08d588975
Nicholas Glesmann <nicholasglesmann@gmail.com>
2019-12-05 16:05:24 -0800

    Add site status routes

--- assets/default-routes.js
+++ assets/default-routes.js
@@ -57,7 +57,7 @@ var test = {
  };
 
  var materials = {
-	 url: 'http://appserver/get-json-materials?sample-event',
+	 url: '/get-json-materials?sample-event',
 	 name: 'materials',
 	 render: show,
 	 shortcut: 'm'// implied Ctrl-m
@@ -90,3 +90,91 @@ var test = {
      form: function(){},
      formCallback: function(){}
  };
+
+ var allSiteStatuses = {
+	name: "all-site-statuses",
+	
+	hasParams: false,
+	
+	headers: {
+		accept: "text/html",
+		contentType: "text/html"
+	},
+
+	// Let's not have to call out to external server, will be nice for tesitng, too.
+	url: "/site-statuses",
+	
+	// Gets passed the body of the Response.
+	render:  function(body){ 
+		return body;
+	},
+	
+	// form: function() {
+	// 	return vNode("div",{"id":"modalContainer"},[vNode("input",{name:"url",id:"urlInput"}, [])]);
+	// },
+	
+	// formCallback:function(){
+    //     var data = document.getElementById("urlInput").value;
+
+	// 	return JSON.stringify({url:data});
+	// }
+};
+
+ var siteStatus = {
+	name: "site-status",
+	
+	hasParams: true,
+	
+	headers: {
+		accept: "application/json",
+		contentType: "text/html"
+	},
+
+	// Let's not have to call out to external server, will be nice for tesitng, too.
+	url: "/site-status",
+	
+	// Gets passed the body of the Response.
+	render:  function(body){ 
+		// body is "You chose..."
+		return vNode("h2",{},body);
+	},
+	
+	form: function() {
+		return vNode("div",{"id":"modalContainer"},[vNode("input",{name:"url",id:"urlInput"}, [])]);
+	},
+	
+	formCallback:function(){
+        var data = document.getElementById("urlInput").value;
+
+		return JSON.stringify({url:data});
+	}
+};
+
+var siteStatusCheckSite = {
+	name: "site-status-check-site",
+	
+	hasParams: false,
+	
+	headers: {
+		accept: "application/json",
+		contentType: "text/html"
+	},
+
+	// Let's not have to call out to external server, will be nice for tesitng, too.
+	url: "/site-status",
+	
+	// Gets passed the body of the Response.
+	render:  function(body){ 
+        console.log(body);
+        return vNode("h2",{},body);
+	},
+	
+	// form: function() {
+	// 	return vNode("div",{"id":"modalContainer"},[vNode("input",{name:"url",id:"urlInput"}, [])]);
+	// },
+	
+	// formCallback:function(){
+	// 	var data = document.getElementById("urlInput").value;
+	// 	return JSON.stringify({url:data});
+	// }
+};
