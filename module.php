fd799772deb639768fece3be6c9ca2722a49bcb2
Jose Bernal <jbernal.web.dev@gmail.com>
2019-12-06 05:51:34 +0000

    Sample text for home.

--- module.php
+++ module.php
@@ -28,6 +28,7 @@ function doAdminPage() {
 	$template = new Template("webconsole");
 	//$template -> addStyles();
 	//$template -> addScript("path to js file");
-	return $template->render(array("content"=> "This is what the page looks like when it is first loaded"));
+	$content = file_get_contents(BASE_PATH ."/content/static/sample.html");
+	return $template->render(array("content"=> $content));
 
 }
