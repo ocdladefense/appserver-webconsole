ab0b694e3d7ba1ea7f8a4f8c947f91149e72870a
Jose Bernal <jbernal.web.dev@gmail.com>
2019-12-20 17:48:05 +0000

    Let client communicate with the worker.

--- assets/lib/client.js
+++ assets/lib/client.js
@@ -1,28 +1,33 @@
 function clientFetchJson(url){
-    return fetch(url, {
-     headers:{'Accept': 'application/json'}, 
-   })
-   .then(function(response) {
-       return response.json();
-   })
-   .then(function(myJson){
-       return myJson;
-   });
-   }
+	return fetch(url, {
+		 headers:{'Accept': 'application/json'}, 
+	 })
+	 .then(function(response) {
+			 return response.json();
+	 })
+	 .then(function(myJson){
+			 return myJson;
+	 });
+}
 
-   function Client(opts){
-       this.isMobile = true;
-       this.hasScreen = true;
-       this.isSpa = true; //spa = single page app
-       this.machineName = "some machine";
-       this.macAddress = "74773685";
-       this.IpAddress = "847q090952";
-       this.features = array();
-   }
+function Client(opts){
+	this.isMobile = true;
+	this.hasScreen = true;
+	this.isSpa = true; //spa = single page app
+	this.machineName = "some machine";
+	this.macAddress = "74773685";
+	this.IpAddress = "847q090952";
+	this.features = array();
+}
    
 
-   window.addEventListener('offline', function(){tellWorker("connected",false);});
-   window.addEventListener('online', function(){tellWorker("connected",true);});
+window.addEventListener('offline', function(){
+	tellWorker("connected",false);
+});
+
+window.addEventListener('online', function(){
+	tellWorker("connected",true);
+});
 
 
 
