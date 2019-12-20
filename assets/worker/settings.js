6c39b04a83a5621dfde79a4280cd721fa77b27be
Jose Bernal <jbernal.web.dev@gmail.com>
2019-12-20 18:25:44 +0000

    Make variables reflect what they do.

--- assets/worker/settings.js
+++ assets/worker/settings.js
@@ -6,31 +6,35 @@ const cacheSettings ={
 
 };
 
-const mydb = {
-    name: "mydb",
-    driver: "DatabaseIndexedDb",
-    schema: {
-        autoIncrement:true,
-        keyPath:"id",
-        indexes:[{name:"name",path:"name", options:{ unique:false }},
-                 {name:"age",path:"age", options:{ unique: false }},
-                 {name:"hairColor",path:"hairColor", options:{ unique: false }}]
-    }
-};
 
-const customers = {
-    name:"customers",
-    driver:"DatabaseIndexDb",
-    schema: {
-        keyPath:"ssn",
-        autoIncrement:false,
-        indexes:[{name:"name",path:"name", options:{ unique:false }},
-                 {name:"email",path:"email", options:{ unique: true }}]
-    }
-};
+
+const storeDefinitions = [
+	{
+		name:"customers",
+		autoIncrement:false,
+		keyPath:"ssn",
+		indexes:[
+			{name:"name",path:"name", options:{ unique:false }},
+			{name:"email",path:"email", options:{ unique: true }}
+		]
+	},
+	{
+		name:"notes",
+		autoIncrement:true,
+		keyPath:"id",
+		indexes:[
+			{name:"name",path:"name", options:{ unique:false }},
+			{name:"age",path:"age", options:{ unique: false }},
+			{name:"hairColor",path:"hairColor", options:{ unique: false }}
+		]
+	}
+];
+
 
 const databaseSettings={
-    name: "my-db-v1",
-    version:1,
-    stores: [mydb, customers]
+	name: "my-db-v1",
+	version:1,
+	driver:"DatabaseIndexedDb",
+	stores: ["customers","notes"], // for this database object only give access to these names object stores.
+	schemas: storeDefinitions // used specifically to create/upgrade the database
 };
