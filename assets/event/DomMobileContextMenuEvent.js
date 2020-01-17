c0c4bedfaaf62ed26bf3f16a473649aa9dbd686f
Nicholas Glesmann <nicholasglesmann@gmail.com>
2020-01-17 13:20:16 -0800

    Basic phone styling and  mobile menu version 1

--- assets/event/DomMobileContextMenuEvent.js
+++ assets/event/DomMobileContextMenuEvent.js
@@ -5,31 +5,44 @@ const DomMobileContextMenuEvent = (function() {
 
 	var proto = {
 		handleEvent: function(e) {
-			console.log(e); // TouchEvent; see also Touch and TouchList objects.
-			 var now, timesince, touchmenu;
+			var now, timesince, touchmenu, elementId;
 	 
-			 touchmenu = document.getElementById("touchmenu");
+			elementId = event.target.id;
 
-	 
-			 now = new Date().getTime();
-	 
-			 if(null == ltap) {
-				 ltap = 0;
-			 }
-	 
-			 timesince = now - ltap;
-	 
-			 if(timesince < DOUBLE_TAP_INTENT_THRESHOLD && timesince > 0){
-				touchmenu.setAttribute("style","position:fixed;top:0px;left:0px;width:100%;height:100%;background-color:blue;z-index:200;");
+			if(elementId === "mobile-icon") {
+				let menus = document.getElementsByClassName("main-menu");
+				let mainMenu = menus[0];
+				mainMenu.classList.toggle("mobile-menu");
+
+				mainMenu.childNodes.forEach((child) => {
+					child.classList.toggle("mobile-menu-element");
+				});
+
+				return false;
+			}
+
+			touchmenu = document.getElementById("touchmenu");
+
+	
+			now = new Date().getTime();
+	
+			if(null == ltap) {
+				ltap = 0;
+			}
+	
+			timesince = now - ltap;
+	
+			if(timesince < DOUBLE_TAP_INTENT_THRESHOLD && timesince > 0){
+				//touchmenu.setAttribute("style","position:fixed;top:0px;left:0px;width:100%;height:100%;background-color:blue;z-index:200;");
 		
 				alert("Double-tap");
 				// double tap
 				ltap = null;
 				return false;
-			 } else {
-				touchmenu.setAttribute("style","display:none;position:fixed;top:0px;left:0px;width:100%;height:100%;background-color:blue;");
-					// too much time to be a doubletap
-			 }
+			} else {
+				//touchmenu.setAttribute("style","display:none;position:fixed;top:0px;left:0px;width:100%;height:100%;background-color:blue;");
+				// too much time to be a doubletap
+			}
 
 			ltap = new Date().getTime();
 			return false;
