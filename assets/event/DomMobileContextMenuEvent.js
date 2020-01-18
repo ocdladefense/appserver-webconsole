c5887160e8aa871f20b9b6fff149569d9099c6d3
Nicholas Glesmann <nicholasglesmann@gmail.com>
2020-01-17 17:16:48 -0800

    Additional styling and menu transition effects

--- assets/event/DomMobileContextMenuEvent.js
+++ assets/event/DomMobileContextMenuEvent.js
@@ -7,18 +7,70 @@ const DomMobileContextMenuEvent = (function() {
 		handleEvent: function(e) {
 			var now, timesince, touchmenu, elementId;
 	 
-			elementId = event.target.id;
+			element = e.target;
 
-			if(elementId === "mobile-icon") {
+			// if the element clicked is the mobile icon, and the mobile menu is not already active, open the mobile menu
+			if(element.id === "mobile-icon" && !element.classList.contains("mobile-menu-active")) { 
 				let menus = document.getElementsByClassName("main-menu");
 				let mainMenu = menus[0];
-				mainMenu.classList.toggle("mobile-menu");
 
-				mainMenu.childNodes.forEach((child) => {
-					child.classList.toggle("mobile-menu-element");
+				element.classList.add("mobile-menu-active");
+
+				mainMenu.classList.add("mobile-menu");
+
+				let slideTime = 0;
+
+				window.setTimeout(() => { mainMenu.classList.add("mobile-menu-slide-in")}, 20);
+
+				mainMenu.childNodes.forEach((menuItem) => { // add necessary classes of child elements
+					menuItem.classList.add("mobile-menu-element");
+
+					window.setTimeout(() => { menuItem.classList.add("mobile-menu-item-slide-in") }, slideTime);
+					menuItem.childNodes.forEach((menuElement) => {
+						if(!menuElement.classList.contains("sub-menu")) {
+							return;
+						}
+						menuElement.childNodes.forEach((subMenuElement) => {
+							subMenuElement.classList.add("sub-menu-element");
+						});
+					});
+					slideTime += 100;
 				});
 
 				return false;
+			} else if(element.nodeName !== "A") { // if the element clicked is not an A tag, close the dropdown menu
+				let menus = document.getElementsByClassName("main-menu");
+				let mainMenu = menus[0];
+				window.setTimeout(() => { mainMenu.classList.remove("mobile-menu-slide-in")}, 100);
+
+				let slideTime = 20;
+				let timeOutTime = 200;
+
+				mainMenu.childNodes.forEach((menuItem) => {
+					window.setTimeout(() => { menuItem.classList.remove("mobile-menu-item-slide-in") }, slideTime);
+				})
+
+				// Remove the mobile-menu-active class from mobile-icon
+				let menuIcon = document.getElementById("mobile-icon");
+				menuIcon.classList.remove("mobile-menu-active");
+				
+				window.setTimeout(() => {
+					mainMenu.classList.remove("mobile-menu");
+
+					mainMenu.childNodes.forEach((menuItem) => {
+						menuItem.classList.remove("mobile-menu-element");
+						menuItem.childNodes.forEach((menuElement) => {
+							if(!menuElement.classList.contains("sub-menu")) {
+								return;
+							}
+							menuElement.childNodes.forEach((subMenuElement) => {
+								subMenuElement.classList.remove("sub-menu-element");
+							});
+						});
+					});
+
+				}, timeOutTime);
+
 			}
 
 			touchmenu = document.getElementById("touchmenu");
